2bd30e09ce103036c876fa6b4928eb1de21cc51b
faisab <59376240+faisab@users.noreply.github.com>
2022-01-05 16:47:25 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,7 @@
 
 
     var database = Firebase.initializeApp({
-      apiKey: 'AIzaSyB9Q7e_H9MPIk2eZdB_TnJArILVMS_Jpp0',
-      authDomain: 'notebook-49e66.firebaseapp.com',
-      databaseURL: 'https://notebook-49e66-default-rtdb.firebaseio.com',
-      projectId: 'notebook-49e66',
-      storageBucket: 'notebook-49e66.appspot.com',
-      messagingSenderId: '934283410857'
+      #api
     }).database().ref();
 
     export default {
